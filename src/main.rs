39f54ac555e3f7aafba79397e4a19564d6bf0fa8
valkyrie_pilot <valk@randomairborne.dev>
2021-09-12 20:41:27 -0600

    Use different types for a very significant performance improvement, add time output

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,18 @@ use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
+use std::time::Instant;
+use num_format::{Locale, ToFormattedString};
 
 fn main() {
-    let mut primes: Vec<i128> = Vec::new();
+    let mut primes: Vec<u64> = Vec::new();
     let mut current_number_being_checked = 1;
     let mut largest_number_wanted_input_vec: Vec<String> = env::args().collect();
     if largest_number_wanted_input_vec.len() != 2 {
-        largest_number_wanted_input_vec.push(1000.to_string())
+        largest_number_wanted_input_vec.push(1000000.to_string())
     }
     let largest_number_wanted_input_string = &largest_number_wanted_input_vec[1];
-    let largest_number_wanted: i128 = match largest_number_wanted_input_string.parse() {
+    let largest_number_wanted: u64 = match largest_number_wanted_input_string.parse() {
         Err(_err) => 100,
         Ok(result) => result,
     };
@@ -23,6 +25,7 @@ fn main() {
     };
     primes.push(2);
     println!("Finding primes...");
+    let before = Instant::now();
     while current_number_being_checked < largest_number_wanted {
         current_number_being_checked = current_number_being_checked + 2;
         for divisor in primes.clone() {
@@ -34,6 +37,7 @@ fn main() {
             }
         }
     }
+    println!("Total time to find {} primes: {:.2?}", primes.len().to_formatted_string(&Locale::en), before.elapsed());
     println!("Finished finding primes, writing to file.");
     let joined: String = primes.iter().map(|&prime| prime.to_string() + "\n").collect();
     match file.write(joined.as_bytes()) {
