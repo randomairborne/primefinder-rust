0fabe8bcb29df872128a7051bb12de88e676cde3
valkyrie_pilot <valk@randomairborne.dev>
2021-09-13 20:24:45 -0600

    Update binaries, autobuild

--- autobuild.sh
+++ autobuild.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo -e "Building for raspberry pi..."
 cross build --release --target armv7-unknown-linux-gnueabihf
+echo -e "Building for arm64 linux..."
+cross build --release --target aarch64-unknown-linux-gnu
 echo -e "Building for M1 macOS"
 cargo build --release --target aarch64-apple-darwin
 echo -e "Building for Intel macOS"
 cargo build --release --target x86_64-apple-darwin
 echo -e "Building for Windows..."
 cargo build --release --target x86_64-pc-windows-gnu
-echo -e "Building for arm64 linux..."
-cargo build --release --target aarch64-unknown-linux-gnu
 echo -e "Building for native..."
 cargo build --release
 cp ./target/armv7-unknown-linux-gnueabihf/release/primesrs ./binaries/raspi-primes.elf
